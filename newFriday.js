8e9129dd884bbefda611d564eb555572e2ccaba8
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-04-17 16:36:20 +0200

    Do restart function and Add sound

--- newFriday.js
+++ newFriday.js
@@ -1,6 +1,8 @@
-const secondDiv = document.getElementById('second-div');
 const scoreDiv = document.getElementById('score-div');
+const showPoints = document.getElementById('points');
+const btnRstart = document.getElementById('btn-rstart')
 const btnStart = document.getElementById('btn-start');
+
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -9,7 +11,7 @@ canvas.height = window.innerHeight / 100 * 80;
 
 let frames = 0;
 let raf;
-const enemies = []
+let enemies = []
 
 const background = new Image();
 background.src = "./images/descarga.jfif";
@@ -22,12 +24,40 @@ arrEnemy.src = "./images/arrow.png";
 
 
 btnStart.addEventListener('click', startGame);
+btnRstart.addEventListener('click', reStartGame);
+
+//Sounds
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+  }
 
 //Start Game
 function startGame(){
     const maindiv = document.getElementById('main-div');
     maindiv.insertBefore(canvas, maindiv.childNodes[0]);
-    secondDiv.style.display = "none";
+    scoreDiv.style.display = "none";
+    btnStart.style.display = "none";
+    updateCanvas()  
+}
+
+function reStartGame(){ 
+    scoreDiv.style.display = "none";
+    btnStart.style.display = "none";
+    enemies = []
+    frames = 0
+    player.x = canvas.width/2,
+    player.y = canvas.height/2,
     updateCanvas()  
 }
 
@@ -102,9 +132,9 @@ class Enemy{
 
 function spawnEnemies(){
         const x = 0;
-        const y = chooseRandom(0, canvas.height);
         const w = 60;
         const h = 40;
+        const y = chooseRandom(0, (canvas.height - h));
         const velocity = 1 /* {
             x:1,
             y:1
@@ -112,6 +142,8 @@ function spawnEnemies(){
         enemies.push(new Enemy(arrEnemy, x, y, w, h, velocity))
  
 }
+let mySound;
+mySound = new sound("./sounds/hit.mp3");
 
 
 //Move Player
@@ -144,23 +176,22 @@ function checkGameOver() {
     });
    
     if (crashed) {
-        console.log('craashed')
+        mySound.play();
         stopGame();
     }
 }
 function stopGame(){
     cancelAnimationFrame(raf);
-    secondDiv.style.display = "flex";
     scoreDiv.style.display = "flex";
 
 }
 
 
 function score(){
-
     const points = Math.floor(frames / 50);
     ctx.font = '18px serif';
     ctx.fillStyle = 'black';
     ctx.fillText(`Score: ${points}`, (canvas.width - 100), 50);
-      
+    showPoints.innerHTML = points;  
 }
+
